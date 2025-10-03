f304162a139faac27e25be06b1cfdf94484311e6
Zach Brose <97310057+zbrose@users.noreply.github.com>
2025-10-02 20:01:05 -0700

    updates

--- Algorithms/arrays/selectionSort.js
+++ Algorithms/arrays/selectionSort.js
@@ -1,16 +1,36 @@
-function selectionSort(nums) {
-  for (let i = 0; i < nums.length - 1; i++) {
+const nums = [21, 6, 7, 4, 23, 5, 87, 3];
+
+// function selectionSort(nums) {
+//   for (let i = 0; i < nums.length - 1; i++) {
+//     let minIndex = i;
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[j] < nums[minIndex]) {
+//         minIndex = j;
+//       }
+//     }
+//     if (minIndex !== i) {
+//       [nums[i], nums[minIndex]] = [nums[minIndex], nums[i]]; // swap values
+//     }
+//   }
+//   return nums;
+// }
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
     let minIndex = i;
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[j] < nums[minIndex]) {
+
+    for (let j = i + 1; j < arr.length; j++) {
+      // this loop sets the new lowest number
+      if (arr[j] < arr[minIndex]) {
         minIndex = j;
       }
     }
+    // if this runs, we got thru the array and there is a new minimum value that is placed in front of our initial minimum value
     if (minIndex !== i) {
-      [nums[i], nums[minIndex]] = [nums[minIndex], nums[i]]; // swap values
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
     }
   }
-  return nums;
+  return arr;
 }
 
 console.log("selection sort:", selectionSort(nums));
